29ff5ec38b068bf40cc5921efb8582a9e62191d3
bawells <bawells@uci.edu>
2018-02-15 22:49:33 -0800

    The version I submitted

--- circles.py
+++ circles.py
@@ -3,38 +3,49 @@ import matplotlib.pyplot as plt
 
 #first 5 bragg condition meeting vectors for the 4 structures of interest
 braggSC =  sp.array([[1,0,0],[1,1,0],[1,1,1],[2,0,0],[2,1,0]])
-braggFCC = sp.array([[1,1,1],[2,0,0],[2,2,0],[2,2,1],[3,1,1]])
+braggFCC = sp.array([[1,1,1],[2,0,0],[2,2,0],[3,1,1],[2,2,2]])
 braggBCC = sp.array([[1,1,0],[2,0,0],[2,1,1],[2,2,0],[3,1,0]])
 braggDia = sp.array([[1,1,1],[2,2,0],[3,1,1],[4,0,0],[3,3,1]])
 
-lamb = 1 # lambda is a keyword, and I'm too lazy for wavelength as a name
+cuKAlpha = 1.542
+unitCella = 5.0
 
-def makecircles(peakArray, name):
+def makecircles(peakArray, wavelength, cellSize,name):
 	circlePlot, ax= plt.subplots()
-	filename = name + '.png' 
-	print( name )
+	filename = name + '.png'
+	f.write( "--------------------- \n" )
+	f.write( name + ':\n' )
+	f.write(' Index   2Theta \n')
+	datalimit = sp.array([0])
 	for peak in peakArray:
-		dhkl = cubicDSpacing(5, peak)
-		angle = sp.arcsin((lamb/ (2 * dhkl)))
+		dhkl = cubicDSpacing(cellSize, peak)
+		angle = sp.arcsin( wavelength / ( 2 * dhkl ) )
 		radius = sp.tan(2 * angle)
-		print( 'Relative radius: ' + str(radius) )
+		f.write( str(peak) + str( 2 * sp.rad2deg(angle) ) + '\n' )
 		circle = plt.Circle((0,0), radius, fill=False)
 		ax.add_artist(circle)
+		if radius > datalimit:
+			datalimit = radius
 	ax.set_aspect('equal', adjustable='datalim')
-	plt.ylim(-1.5,1.5)
-	plt.xlim(-1.5,1.5)
+	graphLimits = datalimit * 1.3 # this magic number is just a format thing
+	plt.xlim(-graphLimits, graphLimits)
+	plt.ylim(-graphLimits, graphLimits)
 	circlePlot.savefig(filename)
-	print( 'Saved as ' + filename )
-	print( "---------------" )
+	f.write( "--------------------- \n\n" )
 
-def cubicDSpacing(unitcell, millerIndex):
+
+def cubicDSpacing(cellSize, millerIndex):
 	h,k,l = millerIndex[[0]], millerIndex[[1]], millerIndex[[2]]
-	dSpacing = unitcell/(( h**2 + k**2 + l**2 )**(1/2))
+	dSpacing = cellSize/( sp.sqrt( h**2 + k**2 + l**2 ) )
 	return dSpacing
 
 
-
-makecircles(braggSC, 'Simple Cubic')
-makecircles(braggFCC, 'Face Centered Cubic')
-makecircles(braggBCC, 'Body Centered Cubic')
-makecircles(braggDia, 'Diamond')
+f = open('Circles','w')
+f.write( 'Assumptions made: \n' )
+f.write( 'Cu Kalpha = ' + str(cuKAlpha) + ' Angstroms \n' )
+f.write( 'Distance to detector = 1 (arbitrary unit)\n' )
+makecircles(braggSC,  cuKAlpha, unitCella, 'Simple Cubic')
+makecircles(braggFCC, cuKAlpha, unitCella, 'Face Centered Cubic')
+makecircles(braggBCC, cuKAlpha, unitCella, 'Body Centered Cubic')
+makecircles(braggDia, cuKAlpha, unitCella, 'Diamond')
+f.close()
